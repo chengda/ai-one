2319b9e723373a53a3f680f02676f9a71fe92f5e
chengda <chengda@joysfintech.com>
2022-11-11 17:56:57 +0800

    引入opencv

--- src/test/java/io/github/ai_one/UnitTest2.java
+++ src/test/java/io/github/ai_one/UnitTest2.java
@@ -3,10 +3,16 @@ package io.github.ai_one;
 import io.github.chengda.ai_one.BpNet;
 import io.github.chengda.ai_one.BpNetTrainer;
 import org.junit.Test;
+import org.opencv.core.*;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UnitTest2 {
     @Test
@@ -21,7 +27,6 @@ public class UnitTest2 {
                 .addExample(createExample("num_train/1.png", 1))
                 .addExample(createExample("num_train/2.png", 2))
                 .addExample(createExample("num_train/3.png", 3))
-                .addExample(createExample("num_train/3.1.png", 3))
                 .addExample(createExample("num_train/4.png", 4))
                 .addExample(createExample("num_train/5.png", 5))
                 .train(1000000, 0.00001);
@@ -30,9 +35,55 @@ public class UnitTest2 {
             BpNet bpNet = BpNet.build(trainer.getModel());
             double[] outputs = bpNet.execute(loadPic("num_test/3.png"));
             System.out.println(outputs[0]);
-            outputs = bpNet.execute(loadPic("num_test/2.png"));
+            outputs = bpNet.execute(loadPic("num_test/4.png"));
             System.out.println(outputs[0]);
         }
+    }
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    @Test
+    public void test2() throws Exception {
+
+        Mat src = Imgcodecs.imread("samples/nums/3.1.png");
+        HighGui.imshow("nums-1", src);
+        HighGui.resizeWindow("nums-1", 400, 300);
+        HighGui.waitKey();
+        Mat gray = new Mat();
+        Imgproc.cvtColor(src, gray, Imgproc.COLOR_RGB2GRAY);
+        HighGui.imshow("nums-2", gray);
+        HighGui.resizeWindow("nums-2", 400, 300);
+        HighGui.waitKey();
+        Mat edges = new Mat();
+        Imgproc.Canny(gray, edges, 80, 100, 3);
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hi = new Mat();
+        Imgproc.findContours(edges, contours, hi, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        System.out.println(contours.size());
+        Rect rect = null;
+        int index = -1;
+        for (int i = 0; i < contours.size(); i++) {
+            Rect tmp = Imgproc.boundingRect(contours.get(i));
+            if (rect == null || tmp.area() > rect.area()) {
+                rect = tmp;
+                index = i;
+            }
+        }
+        MatOfPoint2f mp2f = new MatOfPoint2f(contours.get(index).toArray());
+        RotatedRect rrect = Imgproc.minAreaRect(mp2f);
+        MatOfPoint points=new MatOfPoint();
+        Imgproc.boxPoints(rrect,points);
+        List<MatOfPoint> boxes=new ArrayList<>();
+        boxes.add(points);
+     //   Imgproc.polylines(src,boxes,true,new Scalar(0,255,0),1,Imgproc.LINE_AA);
+        Imgproc.drawContours(src,contours,index,new Scalar(255,0,0),1,Imgproc.LINE_AA);
+        HighGui.imshow("nums-3", src);
+        HighGui.resizeWindow("nums-3", 400, 300);
+        HighGui.waitKey();
+        System.out.println("he");
+
     }
 
     private double[] loadPic(String picSrc) throws Exception {
